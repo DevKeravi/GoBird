799ff8dd8854d7d5295a00b2d1b26bcdeb7fdee4
Keravi <cmkrosp@gmail.com>
2021-10-19 15:06:42 +0900

    FE url 설정

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,10 +1,11 @@
 import { all, fork } from "@redux-saga/core/effects";
 import postSaga from "./post";
 import userSaga from "./user";
+import { backUrl } from "../config/config";
 
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3065";
+axios.defaults.baseURL = backUrl;
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
